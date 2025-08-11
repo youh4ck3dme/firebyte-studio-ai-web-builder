6d1d506c36ebe32ff1ff75febacf8f6d109892d7
h4ck3d.me <h4ck3d@h4ck3d.me>
2025-08-11 21:00:57 +0200

    Add .next/static/chunks/src_app_figma-import_page_tsx_271ad096._.js 1754938856854

---
+++ .next/static/chunks/src_app_figma-import_page_tsx_271ad096._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_2d59be0b._.js",
+  "static/chunks/src_d59b1a15._.js"
+],
+    source: "dynamic"
+});
