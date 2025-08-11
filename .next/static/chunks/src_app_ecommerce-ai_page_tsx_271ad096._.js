7c956993b0490643b5c102743a8c18b6d1a91fee
h4ck3d.me <h4ck3d@h4ck3d.me>
2025-08-11 21:00:44 +0200

    Add .next/static/chunks/src_app_ecommerce-ai_page_tsx_271ad096._.js 1754938843927

---
+++ .next/static/chunks/src_app_ecommerce-ai_page_tsx_271ad096._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_1e4689c8._.js",
+  "static/chunks/src_0f56bb1d._.js"
+],
+    source: "dynamic"
+});
